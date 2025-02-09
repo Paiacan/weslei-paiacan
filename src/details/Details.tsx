517724288edbc86fc894c03f2d28b56586d7ff0d
Arthur Rios Ribeiro <arthurriosribeiro@gmail.com>
2025-02-09 17:29:55 -0300

    Iniciar implementação da página de detalhes

--- src/details/Details.tsx
+++ src/details/Details.tsx
@@ -1,8 +1,15 @@
+import { useParams } from 'react-router-dom';
+import { Product } from '../data/products'
+import products from "../data/products";
+
 const Details : React.FC = () => {
+    const { productId } = useParams<{ productId: string }>();
+
+    const product: Product = products.find((p) => p.id === parseInt(productId!, 10)) as Product;
     return (
         <>
         <div className="w-screen h-screen flex flex-col justify-center items-center">
-            <h1>I ARRIVE ON THE DETAILS PAGE</h1>
+            <h1>I ARRIVE ON THE DETAILS PAGE {product.id}</h1>
         </div>
         </>
     );
